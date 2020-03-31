397ebfdd961d926d47d2f4de3c8987f702ec553b
Ashley Mead <s-meada@bsd405.org>
2020-03-30 22:13:55 -0700

    Add suggestions for items with similar names if requested item is not found

--- Personal Fridge/src/Fridge.java
+++ Personal Fridge/src/Fridge.java
@@ -33,12 +33,14 @@ public class Fridge {
 		Item item = getItemFromName(itemName);
 		if(item == null) {
 			System.out.println("You don't have any " + itemName + " in your fridge.");
+			System.out.println(similarItemsFromNameToString(itemName));
 			return;
 		}
 		
 		if(item.quantity - quantity == 0) {
 			myFridge.remove(getItemIndexFromName(itemName));
 			System.out.println("Removed " + quantity + " " + itemName + " from your fridge.");
+			System.out.println("You don't have any more " + itemName + " in your fridge.");
 		}
 		else if (item.quantity - quantity > 0) {
 			getItemFromName(itemName).subtractFromQuantity(quantity);
@@ -70,10 +72,40 @@ public class Fridge {
 		
 		String fridgeReview = "";
 		for (int itemIndex = 0; itemIndex < myFridge.size(); itemIndex++) {
-			fridgeReview += "|" + myFridge.get(itemIndex).name + ": " + myFridge.get(itemIndex).quantity + "|";
+			Item currentItem = myFridge.get(itemIndex); 
+			fridgeReview += "|" + currentItem.name + ": " + currentItem.quantity + "|";
 		}
 
 		return fridgeReview; // Return a String of all the contents in the fridge and their quantities
+	}
+	
+	// Searches for items with names that contain the given name 
+	// (in case, for example, the user makes a typo or is inconsistent with plurality)
+	public ArrayList<Item> getSimilarItemsFromName(String origName) {
+		ArrayList<Item> possibleItems = new ArrayList<Item>();
+		for(Item item : myFridge) {
+			if (item.name.contains(origName) || origName.contains(item.name)) {
+				possibleItems.add(item);
+			}
+		}
+		return possibleItems;
+	}
+	
+	// toString() method of for the getSimilarItemsFromName() method. Returns a printable String
+	public String similarItemsFromNameToString(String name) {
+		ArrayList<Item> items = getSimilarItemsFromName(name);
+		String returnedStr = "Some items in your fridge you may be looking for: ";
+		// Return an empty string if there are no similar items
+		if(items.isEmpty()) {
+			return "";
+		}
+		else {
+			for(Item item : items) {
+				returnedStr += item.name + ", ";
+			}
+			// Return the string without the last comma and space after the last item
+			return returnedStr.substring(0, returnedStr.length() - 2);
+		}
 	}
 	
 	private Item getItemFromName(String name) {
